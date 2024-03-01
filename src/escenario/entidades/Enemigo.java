c442506621bd03f4d60bbf093eb852dd0e816030
JoelAlexanderProg <joelalexanderprog@gmail.com>
2024-03-01 16:33:18 -0500

    SpritesEnemigos2

--- src/escenario/entidades/Enemigo.java
+++ src/escenario/entidades/Enemigo.java
@@ -19,22 +19,22 @@ public class Enemigo extends Entidad {
     }
 
     public void obtenerImagenDeEnemigo() {
-        arriba1 = configurarImagen("/fuentes/enemigo/enemigo_arriba1");
-        arriba2 = configurarImagen("/fuentes/enemigo/enemigo_arriba2");
-        arriba3 = configurarImagen("/fuentes/enemigo/enemigo_arriba3");
-        arriba4 = configurarImagen("/fuentes/enemigo/enemigo_arriba4");
-        abajo1 = configurarImagen("/fuentes/enemigo/enemigo_abajo1");
-        abajo2 = configurarImagen("/fuentes/enemigo/enemigo_abajo2");
-        abajo3 = configurarImagen("/fuentes/enemigo/enemigo_abajo3");
-        abajo4 = configurarImagen("/fuentes/enemigo/enemigo_abajo4");
-        izquierda1 = configurarImagen("/fuentes/enemigo/enemigo_izquierda1");
-        izquierda2 = configurarImagen("/fuentes/enemigo/enemigo_izquierda2");
-        izquierda3 = configurarImagen("/fuentes/enemigo/enemigo_izquierda3");
-        izquierda4 = configurarImagen("/fuentes/enemigo/enemigo_izquierda4");
-        derecha1 = configurarImagen("/fuentes/enemigo/enemigo_derecha1");
-        derecha2 = configurarImagen("/fuentes/enemigo/enemigo_derecha2");
-        derecha3 = configurarImagen("/fuentes/enemigo/enemigo_derecha3");
-        derecha4 = configurarImagen("/fuentes/enemigo/enemigo_derecha4");
+        arriba1 = configurarImagen("/fuentes/enemigo/ogro_arriba1");
+        arriba2 = configurarImagen("/fuentes/enemigo/ogro_arriba2");
+        arriba3 = configurarImagen("/fuentes/enemigo/ogro_arriba3");
+        arriba4 = configurarImagen("/fuentes/enemigo/ogro_arriba4");
+        abajo1 = configurarImagen("/fuentes/enemigo/ogro_abajo1");
+        abajo2 = configurarImagen("/fuentes/enemigo/ogro_abajo2");
+        abajo3 = configurarImagen("/fuentes/enemigo/ogro_abajo3");
+        abajo4 = configurarImagen("/fuentes/enemigo/ogro_abajo4");
+        izquierda1 = configurarImagen("/fuentes/enemigo/ogro_izquierda1");
+        izquierda2 = configurarImagen("/fuentes/enemigo/ogro_izquierda2");
+        izquierda3 = configurarImagen("/fuentes/enemigo/ogro_izquierda3");
+        izquierda4 = configurarImagen("/fuentes/enemigo/ogro_izquierda4");
+        derecha1 = configurarImagen("/fuentes/enemigo/ogro_derecha1");
+        derecha2 = configurarImagen("/fuentes/enemigo/ogro_derecha2");
+        derecha3 = configurarImagen("/fuentes/enemigo/ogro_derecha3");
+        derecha4 = configurarImagen("/fuentes/enemigo/ogro_derecha4");
     }
 
     public void establecerAcción() {
